ee0806a2a6eb0cb76fd0228925171285cba86d11
Kieran Brown <kb6627500@gmail.com>
2022-04-26 18:40:57 -0400

    Initial Commit

---
+++ Wordle-Guesser/ContentView.swift
@@ -0,0 +1,21 @@
+// Swift toolchain version 5.0
+// Running macOS version 12.3
+// Created on 4/26/22.
+//
+// Author: Kieran Brown
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wordle-Guesser/Wordle_GuesserApp.swift
@@ -0,0 +1,17 @@
+// Swift toolchain version 5.0
+// Running macOS version 12.3
+// Created on 4/26/22.
+//
+// Author: Kieran Brown
+//
+
+import SwiftUI
+
+@main
+struct Wordle_GuesserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
