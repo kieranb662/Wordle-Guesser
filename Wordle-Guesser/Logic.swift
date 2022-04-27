205c88265c27a514617c204283ccf01b12644300
Kieran Brown <kb6627500@gmail.com>
2022-04-27 15:32:52 -0400

    moved best guess function into logic file

--- Wordle-Guesser/Logic.swift
+++ Wordle-Guesser/Logic.swift
@@ -109,4 +109,46 @@ class Game: ObservableObject {
             unguessedLetters: unguessedLetters,
             numberOfUnknownLetters: 5 - numberOfKnownLetters)
     }
+    
+    // For each letter know the number of possible words that contain that letter
+    func bestNextGuess(possibilities: [String], unguessedLetters: String, numberOfUnknownLetters: Int) -> String {
+        var wordsContainingLetter = [String: [String]]()
+        
+        for letter in unguessedLetters {
+            for word in possibilities where word.contains(letter) {
+                if wordsContainingLetter[String(letter)] == nil {
+                    wordsContainingLetter[String(letter)] = [word]
+                } else {
+                    wordsContainingLetter[String(letter)]? += [word]
+                }
+             
+            }
+        }
+        
+        let sortedByMostCommonLetters = wordsContainingLetter
+            .map { (letter: $0, words: $1) }
+            .sorted { previous, next in previous.words.count > next.words.count}
+        
+        let mostCommonLetters = Array(sortedByMostCommonLetters[0..<numberOfUnknownLetters])
+        
+        var bestChoice = ""
+        var currentBestScore = 0
+        
+        for word in possibilities {
+            var amountOfIntersectingPossibilities = 0
+            
+            for (letter, words) in mostCommonLetters where word.contains(letter) {
+                amountOfIntersectingPossibilities += words.count
+            }
+            
+            if amountOfIntersectingPossibilities > currentBestScore {
+                currentBestScore = amountOfIntersectingPossibilities
+                bestChoice = word
+            }
+        }
+        
+        return bestChoice
+    }
+
 }
+
