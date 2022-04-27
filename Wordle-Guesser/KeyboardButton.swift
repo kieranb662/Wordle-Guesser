31e829b4c5b42a5e4f88152043acb9e13669fec7
Kieran Brown <kb6627500@gmail.com>
2022-04-27 15:09:25 -0400

    moved keyboard views into single file

--- Wordle-Guesser/KeyboardButton.swift
+++ Wordle-Guesser/KeyboardButton.swift
@@ -41,6 +41,51 @@ struct KeyboardButton: View {
 }
 
 
+struct KeyboardRow: View {
+    var characters: String
+    var body: some View {
+        HStack(spacing: keyboardButtonSpacing) {
+            ForEach(characters.filter({_ in true}), id: \.self) { char in
+                KeyboardButton(character: String(char))
+            }
+        }
+    }
+}
+
+struct Keyboard: View {
+    let topRow = "qwertyuiop"
+    let middleRow = "asdfghjkl"
+    let bottomRow = "zxcvbnm"
+    
+    var body: some View {
+        VStack(spacing: keyboardButtonSpacing) {
+            
+            KeyboardRow(characters: topRow)
+            KeyboardRow(characters: middleRow)
+            
+            HStack(spacing: keyboardButtonSpacing) {
+                KeyboardRow(characters: bottomRow)
+                
+                Button(action: {
+                    withAnimation(.hideAndShow) {
+                        _ = game.guessCurrentlyEditing.removeLast()
+                    }
+                
+                    LightImpactHaptic()
+                }) {
+                    Image.backspace
+                }
+                .disabled(game.guessCurrentlyEditing.isEmpty)
+                .buttonStyle(KeyboardButtonStyle())
+            }
+        }
+        .transition(.moveDownAndFade)
+    }
+    
+    @EnvironmentObject private var game: Game
+}
+
+
 struct KeyboardButton_Previews: PreviewProvider {
     static var previews: some View {
        KeyboardButton(character: "a")
